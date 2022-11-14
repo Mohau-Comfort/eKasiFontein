3fc320e332c4081873be4c68833cad6ffdb2ed2a
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-11-14 17:29:15 +0200

    updated App. js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StatusBar as ExpoStatusbar } from "expo-status-bar";
 import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
-
 import { ResturantScreen } from "./src/features/resturants/screens/resturants.screen";
 
 export default function App() {
