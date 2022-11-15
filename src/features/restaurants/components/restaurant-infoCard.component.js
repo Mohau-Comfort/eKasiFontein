a4b346f111df3f6a07936f7eaa60aec8dc3fb805
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-11-15 14:59:37 +0200

    updated resturant screen and resturant info card

--- src/features/restaurants/components/restaurant-infoCard.component.js
+++ src/features/restaurants/components/restaurant-infoCard.component.js
@@ -3,8 +3,18 @@ import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-const Title = styled.Text`
+const Title = styled(Text)`
 padding: 16px;
+color: red;
+`;
+
+const RestaurantCard = styled(Card)`
+backgroundColor: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+padding: 20px;
+backgroundColor: white;
 `;
 
 export const RestaurantInfoCard = ({ resturant = {} }) => {
@@ -20,22 +30,9 @@ export const RestaurantInfoCard = ({ resturant = {} }) => {
     } = resturant;
 
     return (
-        <Card elevation={5} style={styles.card}>
-            <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+        <RestaurantCard elevation={5}>
+            <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
             <Title>{name}</Title>
-        </Card>
+        </RestaurantCard>
     )
 }
-
-const styles = StyleSheet.create({
-    card: {
-        backgroundColor: "white",
-    },
-    cover: {
-        padding: 20,
-        backgroundColor: "white",
-    },
-    title: {
-        padding: 16,
-    },
-}); 
