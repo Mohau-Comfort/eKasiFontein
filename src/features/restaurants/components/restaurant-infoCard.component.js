219d4d32aac9d9d78aa954f51c93d4a72f696b98
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-11-16 19:08:14 +0200

    update open.js and restaurant info-card.js

--- src/features/restaurants/components/restaurant-infoCard.component.js
+++ src/features/restaurants/components/restaurant-infoCard.component.js
@@ -3,53 +3,47 @@ import styled from "styled-components/native";
 import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
-import { star } from "../../../../assets/star";
-import { open } from "../../../../assets/open";
-import { Section } from "react-native-paper/lib/typescript/components/Drawer/Drawer";
 
-
-const Address = styled(Text)`
-font-family: ${(props) => props.theme.fonts.heading};
-font-size: ${(props) => props.theme.fontSizes.body};
-`;
-const Title = styled(Text)`
-font-family: ${(props) => props.theme.fonts.heading};
-font-size: ${(props) => props.theme.fontSizes.caption};
-color: ${(props) => props.theme.colors.ui.primary};
-`;
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
-background-color: ${(props) => props.theme.colors.bg.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-padding: ${(props) => props.theme.space[3]};
-background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const Info = styled.View`
-padding: ${(props) => props.theme.space[3]};
-`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
 const Rating = styled.View`
-flex-direction: row;
-padding-top: ${(props) => props.theme.space[2]};
-padding-bottom: ${(props) => props.theme.space[2]};
-`
-const ratingArray = Array.from(new Array(Math.floor(rating)));
-
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
 const Section = styled.View`
-flex-direction: row;
-align-items: center;
+  flex-direction: row;
+  align-items: center;
 `;
-
 const SectionEnd = styled.View`
-flex: 1;
-flex-direction: row;
-justify-content: flex-end;
-`;
-
-const Open = styled(SvgXml)`
-flex-direction: row;
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 export const RestaurantInfoCard = ({ resturant = {} }) => {
@@ -64,6 +58,8 @@ export const RestaurantInfoCard = ({ resturant = {} }) => {
         isClosedTemporarily = true,
     } = resturant;
 
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
@@ -77,17 +73,18 @@ export const RestaurantInfoCard = ({ resturant = {} }) => {
                     </Rating>
                     <SectionEnd>
                         {isClosedTemporarily && (
-                            <Text variant='label' style={{ color: "red" }}>
-                                TEMPORARILY CLOSED
+                            <Text variant="label" style={{ color: "red" }}>
+                                CLOSED TEMPORARILY
                             </Text>
                         )}
                         <View style={{ paddingLeft: 16 }} />
-                        {isOpenNow && <Open xml={open} width={20} height={20} />}
-                        <Image style={{ paddingLeft: 16, width: 15, height: 15 }} sourc={{ uri: icon }} />
+                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+                        <View style={{ paddingLeft: 16 }} />
+                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
                     </SectionEnd>
                 </Section>
                 <Address>{address}</Address>
             </Info>
         </RestaurantCard>
-    )
-}
+    );
+};
