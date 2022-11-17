4f79c053381c660fe4e35ee48d68e2a28f83abfa
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-11-17 08:19:22 +0200

    updated spacer compenent

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,54 +1,31 @@
-import React from "react";
-import { View } from "react-native";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-margin-top: ${(props) => props.theme.space[1]};
-`;
+const sizeVariant = {
+    small: 1,
+    medium: 2,
+    large: 3,
+};
 
-const TopMedium = styled.View`
-margin-top: ${(props) => props.theme.space[2]};
-`;
+const positionVariant = {
+    top: "marginTop",
+    left: "marginLeft",
+    right: "marginRight",
+    bottom: "marginBottom",
+};
 
-const TopLarge = styled.View`
-margin-top: ${(props) => props.theme.space[3]};
-`;
+const getVariant = (position, size, theme) => {
+    const sizeIndex = sizeVariant[size];
+    const property = positionVariant[position];
+    const value = theme.space[sizeIndex];
 
-const LeftSmall = styled.View`
-margin-left: ${(props) => props.theme.space[1]};
-`;
+    return `${property}:${value}`;
+};
 
-const LeftMedium = styled.View`
-margin-left: ${(props) => props.theme.space[2]};
-`;
+export const Spacer = styled.View`
+    ${({ position, size, theme }) => getVariant(position, size, theme)}
+  `;
 
-const LeftLarge = styled.View`
-margin-left: ${(props) => props.theme.space[3]};
-`;
-
-
-export const Spacer = ({ variant }) => {
-
-    if (variant === "top.medium") {
-        return <TopMedium />;
-    }
-
-    if (variant === "top.large") {
-        return <TopLarge />;
-    }
-
-    if (variant === "left.small") {
-        return <LeftSmall />;
-    }
-
-    if (variant === "left.medium") {
-        return <LeftMedium />;
-    }
-
-    if (variant === "left.large") {
-        return <LeftLarge />;
-    }
-
-
-    return <TopSmall />
-}
+Spacer.defaultProps = {
+    position: "top",
+    size: "small",
+};
--- src/features/restaurants/components/restaurant-infoCard.component.js
+++ src/features/restaurants/components/restaurant-infoCard.component.js
@@ -49,43 +49,45 @@ const SectionEnd = styled.View`
 
 export const RestaurantInfoCard = ({ resturant = {} }) => {
 
-    const {
-        name = 'some resturant',
-        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-        photos = ["https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/131974858_114088017220962_2027569032418924384_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=oKIsvzevAkEAX8cXBB2&_nc_ht=scontent-jnb1-1.xx&oh=00_AfDguifOsQap1nyWVfm7kncQPrMrWjyMkK06-TTdfx6WyQ&oe=639AF91C"],
-        address = "100 some random street",
-        isOpenNow = true,
-        rating = 4,
-        isClosedTemporarily = true,
-    } = resturant;
+  const {
+    name = 'some resturant',
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = ["https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/131974858_114088017220962_2027569032418924384_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=oKIsvzevAkEAX8cXBB2&_nc_ht=scontent-jnb1-1.xx&oh=00_AfDguifOsQap1nyWVfm7kncQPrMrWjyMkK06-TTdfx6WyQ&oe=639AF91C"],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = resturant;
 
-    const ratingArray = Array.from(new Array(Math.floor(rating)));
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
-    return (
-        <RestaurantCard elevation={5}>
-            <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Info>
-                <Title>{name}</Title>
-                <Section>
-                    <Rating>
-                        {ratingArray.map(() => (
-                            <SvgXml xml={star} width={20} height={20} />
-                        ))}
-                    </Rating>
-                    <SectionEnd>
-                        {isClosedTemporarily && (
-                            <Text variant="label" style={{ color: "red" }}>
-                                CLOSED TEMPORARILY
-                            </Text>
-                        )}
-                        < Spacer variant="left.large" />
-                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-                        < Spacer variant="left.large" />
-                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-                    </SectionEnd>
-                </Section>
-                <Address>{address}</Address>
-            </Info>
-        </RestaurantCard>
-    );
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            < Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            < Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard >
+  );
 };
