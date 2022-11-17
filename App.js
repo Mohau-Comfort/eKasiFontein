5ced1566535280d103152e1d27cb43f3e57d7633
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-11-17 14:57:20 +0200

    updated Navigation Tab

--- App.js
+++ App.js
@@ -9,16 +9,29 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utility/safe-area.component";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 const Tab = createBottomTabNavigator();
 
-const myTabs = () => {
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "location",
+  Settings: "md-settings",
 
 }
 
 const Settings = () => <SafeArea><Text>Settings</Text></SafeArea>
 const Map = () => <SafeArea><Text>Map</Text></SafeArea>
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
 
@@ -38,7 +51,13 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "aqua",
+              inactiveTintColor: "darkblue",
+            }}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
