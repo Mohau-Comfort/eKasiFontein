f3a43d4262557d4603089bee755b069492a082e1
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-12-12 15:42:59 +0200

    set up stack navigator

--- App.js
+++ App.js
@@ -2,10 +2,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
-import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
+import { useFonts as useOswald, Oswald_400Regular, } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
