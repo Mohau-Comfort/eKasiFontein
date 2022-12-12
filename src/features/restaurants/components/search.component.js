dab0dd18a382f6aeecbc4a5ca12fd4b8f14147d6
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-12-12 12:52:46 +0200

    updated search and location features

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -9,25 +9,25 @@ const SearchContainer = styled.View`
 `;
 
 export const Search = () => {
-    const { keyword, search } = useContext(LocationContext);
-    const [searchKeyword, setSearchKeyword] = useState(keyword);
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
 
-    useEffect(() => {
-        search(searchKeyword);
-    }, []);
+  useEffect(() => {
+    search(searchKeyword);
+  }, []);
 
-    return (
-        <SearchContainer>
-            <Searchbar
-                placeholder="Search for a location"
-                value={searchKeyword}
-                onSubmitEditing={() => {
-                    search(searchKeyword);
-                }}
-                onChangeText={(text) => {
-                    setSearchKeyword(text);
-                }}
-            />
-        </SearchContainer>
-    );
+  return (
+    <SearchContainer>
+      <Searchbar
+        placeholder="Search for a location"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
 };
