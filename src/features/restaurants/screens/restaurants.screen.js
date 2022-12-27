c412c2cf12fd0bc0565cdd78bbbbf39ebfa26db1
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-12-27 16:24:20 +0200

    added animations and favourites screen js in settings component

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,22 +1,16 @@
 import React, { useContext, useState } from "react";
-import { FlatList, StyleSheet, TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import styled from "styled-components";
 import { ActivityIndicator } from "react-native-paper";
-
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { Search } from "../components/search.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
-
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+import { RestaurantList } from "../components/restaurant.list";
+import { FadeInView } from "../../../components/animations/fade.animation";
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
@@ -41,14 +35,17 @@ export const RestaurantsScreen = ({ navigation }) => {
         </LoadingContainer>
       )}
       <Search isFavouritesToggled={isToggled} onFavouritesToggle={() => setIsToggled(!isToggled)} />
-      {isToggled && <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />}
+      {isToggled && (<FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />)}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
           return (
             <TouchableOpacity onPress={() => navigation.navigate("RestaurantDetail", { restaurant: item, })}>
               <Spacer style={styles.setPostion} size="large">
-                <RestaurantInfoCard restaurant={item} />
+                <FadeInView>
+                  <RestaurantInfoCard restaurant={item} />
+                </FadeInView>
+
               </Spacer>
             </TouchableOpacity>
           );
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 
 const SettingsStack = createStackNavigator();
 
@@ -9,7 +10,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         <SettingsStack.Navigator
             headerMode="screen" screenOptions={{ cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS, }} >
             <SettingsStack.Screen options={{ header: () => null, }} name="Settings" component={SettingsScreen} />
-            <SettingsStack.Screen name="Favourites" component={() => null} />
+            <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
         </SettingsStack.Navigator>
     );
 };
